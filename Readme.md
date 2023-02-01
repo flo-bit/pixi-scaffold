1b9d1d32b762965ed190d5b1d1808fa1529512d7
florian <45694132+flo-bit@users.noreply.github.com>
2023-02-01 16:35:51 +0100

    switch to modules

--- Readme.md
+++ Readme.md
@@ -6,4 +6,34 @@ includes:
 
 - automatic resizing of root container (fixed size) and canvas to window size
 - keydown and keyup events
-- setup and animate functions
+- setup and update functions
+
+see [flo-bit.github.io/pixi-scaffold/](https://flo-bit.github.io/pixi-scaffold/) for a simple example.
+
+## Usage
+
+- import the `pixi-scaffold` module
+
+```js
+import PixiScaffold from "https://flo-bit.github.io/pixi-scaffold/pixi-scaffold.js";
+```
+
+- make a game class with a `setup` and `update` function
+
+```js
+class Game {
+  setup(app) {
+    // setup your game here
+  }
+  update(delta, total, app) {
+    // update your game here
+  }
+}
+```
+
+- create a new instance of `PixiScaffold` and pass an instance of the game class
+
+```js
+const game = new Game();
+const app = new PixiScaffold(game);
+```
---
+++ pixi-scaffold.js
@@ -0,0 +1,59 @@
+import * as PIXI from "https://cdn.jsdelivr.net/npm/pixi.js@7.x/dist/pixi.min.mjs";
+
+export default class PixiScaffold {
+  constructor(opts) {
+    this.opts = opts ?? {};
+    window.PIXI = PIXI;
+    this.opts.PS = this;
+    window.PS = this;
+    this.app = new PIXI.Application();
+    this.app.view.style.position = "absolute";
+    this.app.view.style.top = "0px";
+    this.app.view.style.left = "0px";
+    document.body.appendChild(this.app.view);
+
+    this.w = this.w ?? 1000;
+    this.h = this.h ?? 1000;
+    this.root = new PIXI.Container();
+    this.app.stage.addChild(this.root);
+    this.windowResized();
+
+    this.keys = {};
+    document.addEventListener("keydown", this.keyDown.bind(this));
+    document.addEventListener("keyup", this.keyUp.bind(this));
+    window.addEventListener("resize", this.windowResized.bind(this));
+
+    if (this.opts.setup) this.opts.setup(this);
+    let ticker = PIXI.Ticker.shared;
+    ticker.add(this.animate.bind(this));
+  }
+  animate() {
+    /* Update your scene here */
+    let elapsed = PIXI.Ticker.shared.elapsedMS / 1000.0;
+    let total = PIXI.Ticker.shared.lastTime / 1000.0;
+
+    if (this.opts.update) this.opts.update(elapsed, total, this);
+  }
+  resizeRoot() {
+    let w = window.innerWidth,
+      h = window.innerHeight;
+    let scl = Math.min(w / this.w, h / this.h);
+    this.root.position.x = w / 2;
+    this.root.position.y = h / 2;
+    this.root.scale.x = scl;
+    this.root.scale.y = scl;
+  }
+  windowResized() {
+    this.app.renderer.resize(window.innerWidth, window.innerHeight);
+    this.resizeRoot();
+  }
+  keyDown(event) {
+    this.keys[event.key] = true;
+  }
+  keyUp(event) {
+    this.keys[event.key] = false;
+  }
+  addChild(c) {
+    this.root.addChild(c);
+  }
+}
